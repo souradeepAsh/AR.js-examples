6c0f480b60caee5b1e91b2bb028f51aef41c1ed2
KingAsh <71181902+souradeepAsh@users.noreply.github.com>
2023-03-02 20:19:02 +0530

    change done

--- Hit-test.js
+++ Hit-test.js
@@ -37,19 +37,19 @@ renderer = new THREE.WebGLRenderer({
       renderer.xr.enabled = true;
 
       document.body.appendChild(renderer.domElement);
-      
-      document.body.appendChild(
-        ARButton.createButton(renderer, { requiredFeatures: ["hit-test"] })
-      );
+
+      // document.body.appendChild(
+      //   ARButton.createButton(renderer, { requiredFeatures: ["hit-test"] })
+      // );
 
       // For ui Stop AR
-      // document.body.appendChild(
-      //   ARButton.createButton(renderer, {
-      //     requiredFeatures: ["hit-test"],
-      //     optionalFeatures: ["dom-overlay", "dom-overlay-for-handheld-ar"],
-      //     domOverlay: { root: document.body },
-      //   })
-      // );
+      document.body.appendChild(
+        ARButton.createButton(renderer, {
+          requiredFeatures: ["hit-test"],
+          optionalFeatures: ["dom-overlay", "dom-overlay-for-handheld-ar"],
+          domOverlay: { root: document.body },
+        })
+      );
 
       // Only for cube
       const geometry = new THREE.BoxGeometry(1, 1, 1);
