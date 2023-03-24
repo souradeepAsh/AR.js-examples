a77574c6333109c4dcaae4b279dfc85dc6264793
KingAsh <71181902+souradeepAsh@users.noreply.github.com>
2023-03-24 17:31:47 +0530

    change done

--- experiment/main.js
+++ experiment/main.js
@@ -9,6 +9,7 @@ import newData from "./newData.json";
 import { animate } from "./utils";
 import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
 
+
 const state = {
   active: 0,
   data: newData,
@@ -29,18 +30,25 @@ const state = {
   },
 };
 
+let scene, camera, light, container, renderer, controls, objectPlaced = false, clock, controller, reticle, hitTestSource = null, hitTestSourceRequested = false;
+var raycaster, mouse;
+let assests = {};
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-var raycaster, mouse;
+
+clock = new THREE.Clock();
+
 raycaster = new THREE.Raycaster();
+
 mouse = new THREE.Vector2();
-const light = new THREE.AmbientLight(0xffffff); // soft white light
+
+light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
 camera.position.z = 25;
@@ -51,8 +59,6 @@ const draco = new DRACOLoader();
 // draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
 draco.setDecoderPath("./draco/");
 loader.setDRACOLoader(draco);
-
-let assests = {};
 
 const loadAllModels = async () => {
   let models = await Promise.all(
@@ -84,37 +90,8 @@ document.getElementById("prev").addEventListener("click", () => {
   ActivateAnimation();
 });
 
-function ActivateAnimation() {
-  let currentActive = state.getActive();
-  if (anim.length > 0) {
-    anim.forEach((a) => {
-      a.kill();
-    });
-  }
-  anim = [];
-
-  let removedChildren = [];
-  scene.children.forEach((c) => {
-    if (c.type == "Mesh" || c.type == "Group") {
-      removedChildren.push(c);
-    }
-  });
-
-  removedChildren.forEach((c) => {
-    scene.remove(c);
-  });
-
-  currentActive.models.forEach((m) => {
-    let mesh = assests[m.name];
-    anim.push(animate(mesh, "position", 1, m.position));
-    anim.push(animate(mesh, "rotation", 1, m.spin));
-    anim.push(animate(mesh, "scale", 1, m.scale));
-    scene.add(mesh);
-  });
-}
-
 // ============================================================
-const renderer = new THREE.WebGLRenderer({
+renderer = new THREE.WebGLRenderer({
   antialias: true,
   alpha: true,
 });
@@ -124,7 +101,7 @@ document.body.appendChild(renderer.domElement);
 renderer.xr.enabled = true;
 
 // orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
+controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 // renderer.domElement.addEventListener("click", onClick, false);
 
@@ -136,9 +113,87 @@ document.body.appendChild(
   })
 );
 
-// render loop
-function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
+//On select function for the Object Place
+function onSelect(){
+  if (objectPlaced) return;
+
 }
-render();
+
+controller = renderer.xr.getController(0);
+controller.addEventListener("select", onSelect);
+scene.add(controller);
+
+reticle = new THREE.Mesh(
+  new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+  new THREE.MeshBasicMaterial()
+);
+reticle.matrixAutoUpdate = false;
+reticle.visible = false;
+scene.add(reticle);
+
+//animate function for animation play
+function ActivateAnimation() {
+  let currentActive = state.getActive();
+  if (anim.length > 0) {
+    anim.forEach((a) => {
+      a.kill();
+    });
+  }
+  anim = [];
+  let removedChildren = [];
+  scene.children.forEach((c) => {
+    if (c.type == "Mesh" || c.type == "Group") {
+      removedChildren.push(c);
+    }
+  });
+  removedChildren.forEach((c) => {
+    scene.remove(c);
+  });
+
+  currentActive.models.forEach((m) => {
+    let mesh = assests[m.name];
+    anim.push(animate(mesh, "position", 1, m.position));
+    anim.push(animate(mesh, "rotation", 1, m.spin));
+    anim.push(animate(mesh, "scale", 1, m.scale));
+    scene.add(mesh);
+  });
+  renderer.setAnimationLoop(render);
+}
+ActivateAnimation();
+
+// render loop
+function render( timestamp, frame ) {
+  const delta = clock.getDelta();
+  if ( frame ) {
+    const referenceSpace = renderer.xr.getReferenceSpace();
+    const session = renderer.xr.getSession();
+    if (hitTestSourceRequested === false && !objectPlaced) {
+      session
+        .requestReferenceSpace("viewer")
+        .then(function (referenceSpace) {
+          return session.requestHitTestSource({ space: referenceSpace });
+        })
+        .then(function (source) {
+          hitTestSource = source;
+        });
+      session.addEventListener("end", function () {
+        hitTestSourceRequested = false;
+        hitTestSource = null;
+      });
+      hitTestSourceRequested = true;
+    }
+    if (hitTestSource && !objectPlaced) {
+      const hitTestResults = frame.getHitTestResults(hitTestSource);
+      if (hitTestResults.length) {
+        const hit = hitTestResults[0];
+        const position = new THREE.Vector3();
+        position.fromArray(hit.getPose(referenceSpace).transform.position);
+        reticle.visible = true;
+        reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+      } else {
+        reticle.visible = false;
+      }
+    }
+  }
+  renderer.render( scene, camera );
+}
